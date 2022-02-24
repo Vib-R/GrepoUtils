d2898ee27e8b8939c1cb2397a2c070503793d0c0
Inkei Márk <m.mark.inkei@gmail.com>
2022-02-24 09:06:50 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -5,9 +5,9 @@ GM_config.init(
   {
     'Status':
     {
+      'label': 'TőzsdeBot', // Appears next to field
       'section': ['Personal Info About Yourself', 
        'We need this info to do stuff'], // Appears above the field
-      'label': 'TőzsdeBot', // Appears next to field
       'type': 'checkbox', // Makes this setting a checkbox input
       'default': false // Default value if user doesn't change it
     }
